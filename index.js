b4ea0bd56b35f9ffb3b9d05bc87e3231041a2caa
Zachary J. Myers <zmyers550@gmail.com>
2018-10-15 13:31:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-const { Client, Attachment } = require('discord.js');
+const {
+  Client,
+  Attachment
+} = require('discord.js');
 const https = require('https');
+const snek = require('snekfetch');
 const client = new Client();
 const prefix = 'hack!';
 
 client.on('ready', () => console.log('Bot has logged in!'));
 
 client.on('message', async (msg) => {
-  if(msg.author.bot || !msg.content.startsWith(prefix)) return;
+  if (msg.author.bot || !msg.content.startsWith(prefix)) return;
   let args = msg.content.split(' ');
   const command = args.shift().slice(prefix.length);
   //Add your commands here. Good command handlers are overrated :POGGERS:
 
-  if(command === 'ping'){
+  if (command === 'ping') {
     const m = await msg.channel.send('Pinging...');
     m.edit(`Ponggers! Client ping: \`${m.createdTimestamp - msg.createdTimestamp}\`ms. Heartbeat ping: \`${client.ping}\`ms`);
-  } else if(command === 'hello') {
+  } else if (command === 'hello') {
     await msg.reply(`Hello ${msg.author.username}`);
-  } else if(command === 'steak'){
+  } else if (command === 'steak') {
     let attach = new Attachment("https://media.giphy.com/media/9UyZI216ic5vG/giphy.gif");
     msg.channel.send(attach);
-  } else if(command === 'lul') {
+  } else if (command === 'lul') {
     const attachment = new Attachment('https://ubisafe.org/images/lul-transparent-twitch-1.png');
     msg.channel.send(attachment);
-  } else if(command === 'status') {
+  } else if (command === 'status') {
     const m = await msg.channel.send('Querying GitHub for pull request count during Hacktoberfest...');
     const username = args[0];
     const apiUrl = `https://api.github.com/search/issues?q=-label:invalid+created:2018-09-30T10%3A00%3A00%2B00%3A00..2018-11-01T12%3A00%3A00%2B00%3A00+type:pr+is:public+author:${username}&per_page=300`;
@@ -38,15 +42,26 @@ client.on('message', async (msg) => {
       resp.on('end', () => {
         const result = JSON.parse(data);
 
-        if(result.errors) {
+        if (result.errors) {
           m.edit(`${username} not found :(`);
         } else {
           m.edit(`${username} made ${result.total_count}/5 pull requests during Hacktoberfest 2018`);
         }
       });
     });
-  } else if(command === 'scream') {
+  } else if (command === 'scream') {
     msg.reply(args.join(' ').toUpperCase());
+  } else if (command === 'cat') {
+    try {
+      const {body} = await snek
+        .get('http://aws.random.cat/meow');
+      const embed = new Discord.RichEmbed()
+        .setColor(0x00A2E8)
+        .setImage(body.file);
+      message.channel.send({embed})
+    } catch (err) {
+      console.log(err.stack)
+    }
   }
 })
 
